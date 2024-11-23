fddafc02cdf2a8c71cc9f0330b68cbabe07317af
Denial-Derman <163509136+Denial-Derman@users.noreply.github.com>
2024-11-23 21:31:41 +0200

    Delete ІДЗ/utils.py

--- ІДЗ/utils.py
+++
@@ -1,7 +0,0 @@
-# Перевірка, чи введено число
-def get_float_input(prompt):
-    while True:
-        try:
-            return float(input(prompt))
-        except ValueError:
-            print("Введено некоректне значення, спробуйте ще раз.")
